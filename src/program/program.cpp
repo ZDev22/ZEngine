7f0b93c8399210392fa27f9fb9816e1a4c7c5dcc
ZDev <adamorrtexas@gmail.com>
2025-07-06 14:43:33 -0500

    smol tweaks

--- src/program/program.cpp
+++ src/program/program.cpp
@@ -3,8 +3,8 @@
 #include "../vulkan/global.hpp"
 #include "functions/keyboard.hpp"
 
-// #define MINIAUDIO_IMPLEMENTATION
-// #include "functions/deps/miniaudio.h"
+#define MA_IMPLEMENTATION
+#include "functions/deps/miniaudio.h"
 
 #include <glm/glm.hpp>
 
@@ -12,18 +12,14 @@
 #include <vector>
 #include <string>
 
-Program::Program(Keyboard& keyboard) : keyboard(keyboard) {
-    // ma_engine engine;
-    // ma_engine_init(NULL, &engine);
-    // ma_sound sound;
-}
+Program::Program(Keyboard& keyboard) : keyboard(keyboard) {}
 
 float speedY = 0.f;
 
 void Program::tick() {
     speedY += 2.9f * deltaTime;
     sprites[0].translation.y += speedY * deltaTime;
-    sprites[0].rotation -= .1f;
+    sprites[0].rotation -= 1.f * deltaTime;
     std::vector<int> keys = {GLFW_KEY_SPACE, GLFW_KEY_A, GLFW_KEY_S, GLFW_KEY_D};
     if (keyboard.keyHit(keys[0])) {
         speedY = -1.5f;
@@ -36,10 +32,6 @@ void Program::tick() {
             sprites[i].translation.x = 1.5f;
         }
     }
-
-
-    // ma_sound_init_from_file(&engine, "audio.wav", 0, NULL, NULL, &sound);
-    // ma_sound_start(&sound);
 
     //Reset stuff for next frame
     keyboard.resetKeys();
