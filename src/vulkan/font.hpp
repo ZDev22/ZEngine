d3910a556a40054d6f5f2f193b8e96478d814873
ZDev22 <adamorrtexas@gmail.com>
2025-07-15 14:14:02 -0500

    i guess i can do this?

--- src/vulkan/font.hpp
+++ src/vulkan/font.hpp
@@ -11,11 +11,5 @@
 #include <memory>
 
 class Pipeline;
-struct Font {
-    stbtt_fontinfo info;
-    std::vector<unsigned char> ttfBuffer;
-    float scale;
-    int ascent, descent, lineGap;
-};
 
 std::unique_ptr<Texture> createFontTexture(Device& device, Pipeline& pipeline, const std::string& ttfPath, float pixelHeight, int atlasSize, VkDescriptorSetLayout descriptorSetLayout, VkDescriptorPool descriptorPool);
