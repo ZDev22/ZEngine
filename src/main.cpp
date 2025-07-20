3bba42b23202744ce4d2a37d46f2a9b2d60f7122
ZDev22 <adamorrtexas@gmail.com>
2025-07-20 15:52:15 -0500

    make startup much faster

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,10 @@
 void error_callback(int error, const char* description) { std::cerr << "GLFW Error (" << error << "): " << description << std::endl; }
 int main() {
     compile();
+    
     glfwSetErrorCallback(error_callback);
-    std::cout << "Calling glfwInit()..." << std::endl;
     if (!glfwInit()) { std::cout << "GLFW failed to initialize!" << std::endl; return 1; }
     std::cout << "GLFW initialized successfully!" << std::endl;
-    if (!glfwVulkanSupported()) { std::cerr << "Vulkan is NOT supported!" << std::endl; return 1; }
-    std::cout << "GLFW Vulkan is supported." << std::endl;
 
     App app{};
     app.run();
