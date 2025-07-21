96289de1ca04d2c0e58f9161287064e9236d316b
ZDev22 <adamorrtexas@gmail.com>
2025-07-21 17:44:22 -0500

    asyncronus texture loading (for better startup time)

--- src/vulkan/pipeline.cpp
+++ src/vulkan/pipeline.cpp
@@ -11,6 +11,7 @@
 #include <fstream>
 #include <cassert>
 #include <array>
+#include <future> // Added for std::async
 
 std::vector<std::unique_ptr<Texture>> spriteTextures;
 
@@ -37,24 +38,15 @@ std::vector<char> Pipeline::readFile(const std::string& filepath) {
 }
 
 void Pipeline::createGraphicsPipeline(const std::string& vertFilepath, const std::string& fragFilepath) {
-    std::cout << "[Pipeline] Reading vertex shader: " << vertFilepath << std::endl;
-    vertShaderModule = createShaderModule(readFile(vertFilepath));
-    std::cout << "[Pipeline] Vertex shader loaded and module created" << std::endl;
-
-    std::cout << "[Pipeline] Reading fragment shader: " << fragFilepath << std::endl;
-    fragShaderModule = createShaderModule(readFile(fragFilepath));
-    std::cout << "[Pipeline] Fragment shader loaded and module created" << std::endl;
-
     VkPipelineShaderStageCreateInfo shaderStages[2] = {};
     shaderStages[0].sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
     shaderStages[0].stage = VK_SHADER_STAGE_VERTEX_BIT;
-    shaderStages[0].module = vertShaderModule;
+    shaderStages[0].module = createShaderModule(readFile(vertFilepath));
     shaderStages[0].pName = "main";
     shaderStages[1].sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
     shaderStages[1].stage = VK_SHADER_STAGE_FRAGMENT_BIT;
-    shaderStages[1].module = fragShaderModule;
+    shaderStages[1].module = createShaderModule(readFile(fragFilepath));
     shaderStages[1].pName = "main";
-    std::cout << "[Pipeline] Shader stages created" << std::endl;
 
     VkVertexInputBindingDescription bindingDescription = Model::Vertex::getBindingDescription();
     auto attributeDescriptions = Model::Vertex::getAttributeDescriptions();
@@ -64,19 +56,16 @@ void Pipeline::createGraphicsPipeline(const std::string& vertFilepath, const std
     vertexInputInfo.pVertexBindingDescriptions = &bindingDescription;
     vertexInputInfo.vertexAttributeDescriptionCount = static_cast<uint32_t>(attributeDescriptions.size());
     vertexInputInfo.pVertexAttributeDescriptions = attributeDescriptions.data();
-    std::cout << "[Pipeline] Vertex input state created" << std::endl;
 
     VkPipelineInputAssemblyStateCreateInfo inputAssembly{};
     inputAssembly.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
     inputAssembly.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_STRIP;
     inputAssembly.primitiveRestartEnable = VK_FALSE;
-    std::cout << "[Pipeline] Input assembly state created" << std::endl;
 
     VkPipelineViewportStateCreateInfo viewportState{};
     viewportState.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
     viewportState.viewportCount = 1;
     viewportState.scissorCount = 1;
-    std::cout << "[Pipeline] Viewport state created" << std::endl;
 
     VkPipelineRasterizationStateCreateInfo rasterizer{};
     rasterizer.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
@@ -87,13 +76,11 @@ void Pipeline::createGraphicsPipeline(const std::string& vertFilepath, const std
     rasterizer.cullMode = VK_CULL_MODE_NONE;
     rasterizer.frontFace = VK_FRONT_FACE_CLOCKWISE;
     rasterizer.depthBiasEnable = VK_FALSE;
-    std::cout << "[Pipeline] Rasterization state created" << std::endl;
 
     VkPipelineMultisampleStateCreateInfo multisampling{};
     multisampling.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
     multisampling.sampleShadingEnable = VK_FALSE;
     multisampling.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
-    std::cout << "[Pipeline] Multisample state created" << std::endl;
 
     VkPipelineColorBlendAttachmentState colorBlendAttachment{};
     colorBlendAttachment.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
@@ -110,7 +97,6 @@ void Pipeline::createGraphicsPipeline(const std::string& vertFilepath, const std
     colorBlending.logicOpEnable = VK_FALSE;
     colorBlending.attachmentCount = 1;
     colorBlending.pAttachments = &colorBlendAttachment;
-    std::cout << "[Pipeline] Color blend state created" << std::endl;
 
     VkPipelineDepthStencilStateCreateInfo depthStencil{};
     depthStencil.sType = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO;
@@ -119,19 +105,18 @@ void Pipeline::createGraphicsPipeline(const std::string& vertFilepath, const std
     depthStencil.depthCompareOp = VK_COMPARE_OP_LESS;
     depthStencil.depthBoundsTestEnable = VK_FALSE;
     depthStencil.stencilTestEnable = VK_FALSE;
-    std::cout << "[Pipeline] Depth-stencil state created" << std::endl;
 
     std::vector<VkDynamicState> dynamicStates = { VK_DYNAMIC_STATE_VIEWPORT, VK_DYNAMIC_STATE_SCISSOR };
     VkPipelineDynamicStateCreateInfo dynamicState{};
     dynamicState.sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
     dynamicState.dynamicStateCount = static_cast<uint32_t>(dynamicStates.size());
     dynamicState.pDynamicStates = dynamicStates.data();
-    std::cout << "[Pipeline] Dynamic state created" << std::endl;
 
     if (texturePaths.empty()) {
         std::cerr << "[Pipeline][ERROR] texturePaths is empty! Cannot create descriptor set layout." << std::endl;
         throw std::runtime_error("No textures provided for pipeline!");
     }
+
     VkDescriptorSetLayoutBinding bufferBinding{};
     bufferBinding.binding = 0;
     bufferBinding.descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
@@ -151,13 +136,10 @@ void Pipeline::createGraphicsPipeline(const std::string& vertFilepath, const std
     layoutInfo.bindingCount = static_cast<uint32_t>(bindings.size());
     layoutInfo.pBindings = bindings.data();
 
-    VkResult res = vkCreateDescriptorSetLayout(device.device(), &layoutInfo, nullptr, &descriptorSetLayout);
-    std::cout << "[Pipeline] vkCreateDescriptorSetLayout returned: " << res << std::endl;
-    if (res != VK_SUCCESS) {
-        std::cerr << "[Pipeline][ERROR] Failed to create descriptor set layout! VkResult: " << res << std::endl;
+    if (vkCreateDescriptorSetLayout(device.device(), &layoutInfo, nullptr, &descriptorSetLayout) != VK_SUCCESS) {
+        std::cerr << "[Pipeline][ERROR] Failed to create descriptor set layout! VkResult:" << std::endl;
         throw std::runtime_error("failed to create descriptor set layout!");
     }
-    std::cout << "[Pipeline] Descriptor set layout created: " << descriptorSetLayout << std::endl;
 
     VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
     pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
@@ -170,13 +152,10 @@ void Pipeline::createGraphicsPipeline(const std::string& vertFilepath, const std
     pipelineLayoutInfo.pushConstantRangeCount = 1;
     pipelineLayoutInfo.pPushConstantRanges = &pushConstantRange;
 
-    res = vkCreatePipelineLayout(device.device(), &pipelineLayoutInfo, nullptr, &pipelineLayout);
-    std::cout << "[Pipeline] vkCreatePipelineLayout returned: " << res << std::endl;
-    if (res != VK_SUCCESS) {
-        std::cerr << "[Pipeline][ERROR] Failed to create pipeline layout! VkResult: " << res << std::endl;
+    if (vkCreatePipelineLayout(device.device(), &pipelineLayoutInfo, nullptr, &pipelineLayout) != VK_SUCCESS) {
+        std::cerr << "[Pipeline][ERROR] Failed to create pipeline layout! VkResult:" << std::endl;
         throw std::runtime_error("failed to create pipeline layout!");
     }
-    std::cout << "[Pipeline] Pipeline layout created with descriptor set layout: " << descriptorSetLayout << std::endl;
 
     std::array<VkDescriptorPoolSize, 2> poolSizes{};
     poolSizes[0].type = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
@@ -190,13 +169,10 @@ void Pipeline::createGraphicsPipeline(const std::string& vertFilepath, const std
     poolInfo.pPoolSizes = poolSizes.data();
     poolInfo.maxSets = 1;
 
-    res = vkCreateDescriptorPool(device.device(), &poolInfo, nullptr, &descriptorPool);
-    std::cout << "[Pipeline] vkCreateDescriptorPool returned: " << res << std::endl;
-    if (res != VK_SUCCESS) {
-        std::cerr << "[Pipeline][ERROR] Failed to create descriptor pool! VkResult: " << res << std::endl;
+    if (vkCreateDescriptorPool(device.device(), &poolInfo, nullptr, &descriptorPool) != VK_SUCCESS) {
+        std::cerr << "[Pipeline][ERROR] Failed to create descriptor pool! VkResult:" << std::endl;
         throw std::runtime_error("failed to create descriptor pool!");
     }
-    std::cout << "[Pipeline] Descriptor pool created: " << descriptorPool << std::endl;
 
     VkGraphicsPipelineCreateInfo pipelineInfo{};
     pipelineInfo.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
@@ -215,10 +191,8 @@ void Pipeline::createGraphicsPipeline(const std::string& vertFilepath, const std
     pipelineInfo.subpass = 0;
     pipelineInfo.basePipelineHandle = VK_NULL_HANDLE;
 
-    res = vkCreateGraphicsPipelines(device.device(), VK_NULL_HANDLE, 1, &pipelineInfo, nullptr, &graphicsPipeline);
-    std::cout << "[Pipeline] vkCreateGraphicsPipelines returned: " << res << std::endl;
-    if (res != VK_SUCCESS) {
-        std::cerr << "[Pipeline][ERROR] Failed to create graphics pipeline! VkResult: " << res << std::endl;
+    if (vkCreateGraphicsPipelines(device.device(), VK_NULL_HANDLE, 1, &pipelineInfo, nullptr, &graphicsPipeline) != VK_SUCCESS) {
+        std::cerr << "[Pipeline][ERROR] Failed to create graphics pipeline!" << std::endl;
         throw std::runtime_error("failed to create graphics pipeline!");
     }
     std::cout << "[Pipeline] Graphics pipeline created successfully." << std::endl;
@@ -302,10 +276,13 @@ void Pipeline::createTextSprites(const std::string& text, glm::vec2 position, fl
 
 void Pipeline::loadSprites() {
     std::cout << "Starting sprite loading...\n";
-    spriteTextures.reserve(texturePaths.size());
-    for (size_t f = 0; f < texturePaths.size(); f++) {
-        spriteTextures.push_back(std::make_unique<Texture>(device, texturePaths[f], descriptorSetLayout, descriptorPool, *this));
-    }
+    spriteTextures.clear();
+    spriteTextures.reserve(texturePaths.size() + 1);
+
+    std::vector<std::future<std::unique_ptr<Texture>>> futures;
+    for (const auto& path : texturePaths) { futures.push_back(std::async(std::launch::async, [this, &path]() { return std::make_unique<Texture>(device, path, descriptorSetLayout, descriptorPool, *this); })); }
+    for (auto& fut : futures) { spriteTextures.push_back(fut.get()); }
+
     spriteTextures.push_back(createFontTexture(device, *this, "fonts/Bullpen3D.ttf", 32.f, 512, descriptorSetLayout, descriptorPool, fontCharData));
 
     sprites.clear();
@@ -327,6 +304,5 @@ void Pipeline::loadSprites() {
     }
 
     //createTextSprites("Hello", glm::vec2(0.0f, 0.0f), 0.1f, glm::vec4(1.0f), spriteTextures.size() - 1);
-
     std::cout << "Sprites created: " << sprites.size() << std::endl;
 }
