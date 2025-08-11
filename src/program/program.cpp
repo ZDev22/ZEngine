f29a1e4aeab69755164e56f55fd07da3112217bb
ZDev <adamorrtexas@gmail.com>
2025-08-10 20:42:46 -0500

    kill bird if offscreen, and don't start until you press space so low-end devices don't kill you on startup

--- src/program/program.cpp
+++ src/program/program.cpp
@@ -18,47 +18,57 @@ Program::Program(Keyboard& keyboard, Pipeline& pipeline, Push& push) : keyboard(
 
 float speedY = 0.f;
 bool dead = false;
+bool started = false;
 int score = 0;
 
 void Program::tick() {
-    speedY += 3.8f * deltaTime;
-    sprites[0].position.y += speedY * deltaTime;
-    sprites[0].rotation -= 90.f * deltaTime;
-    if (!dead && keyboard.keyHit(GLFW_KEY_SPACE)) {
-        speedY = -1.3f;
-        sprites[0].rotation = 60.f;
-        sprites[0].textureIndex = 0;
+    if (started) {
+        speedY += 3.8f * deltaTime;
+        sprites[0].position.y += speedY * deltaTime;
+        sprites[0].rotation -= 90.f * deltaTime;
+        if (!dead) {
+            if (keyboard.keyHit(GLFW_KEY_SPACE)) {
+                speedY = -1.3f;
+                sprites[0].rotation = 60.f;
+                sprites[0].textureIndex = 0;
 
-        audio.play("chirp.mp3");
+                audio.play("chirp.mp3");
+            }
+
+            if (sprites[0].position.y > 1.f || sprites[0].position.y < -1.f) {
+                dead = true;
+                audio.play("hit.mp3");
+            }
+        }
+        else if (sprites[0].position.y > 2.5f) {
+            sprites[0].textureIndex = 0;
+            sprites[0].position.y = -.25f;
+            sprites[0].rotation = 0.f;
+            speedY = 0.f;
+            dead = false;
+        }
     }
+    else { if (keyboard.keyPressed(GLFW_KEY_SPACE)) { started = true; }}
 
     for (size_t i = 1; i < (sprites.size()) / 2; i++) {
-        size_t index = ((i - 1) * 2) + 1;
-        sprites[index].position.x -= .5f * deltaTime;
-        if (sprites[index].position.x < -1.5f) {
-            sprites[index].position.x = 1.5f;
-            sprites[index].position.y = randomFloat(.4f, 1.4f);
+            size_t index = ((i - 1) * 2) + 1;
+            sprites[index].position.x -= .5f * deltaTime;
+            if (sprites[index].position.x < -1.5f) {
+                sprites[index].position.x = 1.5f;
+                sprites[index].position.y = randomFloat(.4f, 1.4f);
+            }
+            sprites[index + 1].position.x = sprites[index].position.x;
+            sprites[index + 1].position.y = sprites[index].position.y - 2.f;
+
+            if (!dead && (checkCollision(spriteCPU[index], sprites[index], spriteCPU[0], sprites[0]) || checkCollision(spriteCPU[index + 1], sprites[index + 1], spriteCPU[0], sprites[0]))) {
+                dead = true;
+                speedY = -1.5f;
+                sprites[0].rotation = 60.f;
+                sprites[0].textureIndex = 1;
+
+                audio.play("hit.mp3");
+            }
         }
-        sprites[index + 1].position.x = sprites[index].position.x;
-        sprites[index + 1].position.y = sprites[index].position.y - 2.f;
-
-        if (!dead && (checkCollision(spriteCPU[index], sprites[index], spriteCPU[0], sprites[0]) || checkCollision(spriteCPU[index + 1], sprites[index + 1], spriteCPU[0], sprites[0]))) {
-            dead = true;
-            speedY = -1.5f;
-            sprites[0].rotation = 60.f;
-            sprites[0].textureIndex = 1;
-
-            audio.play("hit.mp3");
-        }
-    }
-
-    if (dead && sprites[0].position.y > 2.5f) {
-        sprites[0].textureIndex = 0;
-        sprites[0].position.y = -.25f;
-        sprites[0].rotation = 0.f;
-        speedY = 0.f;
-        dead = false;
-    }
 
     //Reset stuff for next frame
     keyboard.resetKeys();
